65470138b6ac858243657d2931fbb765f389f48f
visheshkhurana <vysheshk@gmail.com>
2026-03-23 06:40:51 +0400

    Translate Step4Report.vue: Chinese to English

--- frontend/src/components/Step4Report.vue
+++ frontend/src/components/Step4Report.vue
@@ -58,7 +58,7 @@
                       <path d="M12 2a10 10 0 0 1 10 10" stroke-width="4" stroke="#4B5563" stroke-linecap="round"></path>
                     </svg>
                   </div>
-                  <span class="loading-text">正在生成{{ section.title }}...</span>
+                  <span class="loading-text">Generating{{ section.title }}...</span>
                 </div>
               </div>
             </div>
